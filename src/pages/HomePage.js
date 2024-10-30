33bc6078173f7f146e9aa5395e63214bc86884ee
tanishi123 <tanishi.c2003@gmail.com>
2024-10-30 16:26:46 +0530

    change

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,16 +7,17 @@ const HomePage = () => {
     <Box className="homepage">
       <Box className="hero">
         <Heading><h1>Welcome to the Next-Gen Quiz App</h1></Heading>
-        <Text mt={4}>
+        <p><h2>Your AI Assissted Classroom with Quiz Generation and Assessment</h2></p>
+        <Text mt={4}><p>
           <h1>This is your dashboard where you can upload study materials, take quizzes, and view your grades. 
-          Our app uses advanced NLP technology to create quizzes from your study materials, making learning more effective and interactive.</h1>
+          Our app uses advanced NLP technology to create quizzes from your study materials, making learning more effective and interactive.</h1></p>
         </Text>
       </Box>
       <Box p={5}>
         <Heading><h1>Get Started</h1></Heading>
-        <Text mt={4}><h3>
+        <Text mt={4}><h3><p>
           Begin by uploading your study materials and let our app generate quizzes for you. 
-          Track your progress and manage your learning efficiently with our user-friendly interface.</h3>
+          Track your progress and manage your learning efficiently with our user-friendly interface.</p></h3>
         </Text>
       </Box>
     </Box>
